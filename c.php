8a5394e6f3a5e25575828bed3ddf568ca8a57274
kushagrakhare <kushagrakhare1991@gmail.com>
2016-03-30 15:59:42 +0530

    Added files via upload

---
+++ c.php
@@ -0,0 +1,52 @@
+{
+"quiz_questions":
+[
+{
+"id":"1","question":"In which decade was the American Institute of Electrical Engineers (AIEE) founded?",
+"possible_answers":"4, 1, 2, 3","correct_answer":"1"
+},
+
+{
+"id":"2","question":"	
+What is part of a database that holds only one type of information?",
+"possible_answers":"1, 2, 4, 3","correct_answer":"3"
+}
+,
+
+{
+"id":"3","question":"'OS' computer abbreviation usually means ?",
+"possible_answers":"1, 2, 4, 3","correct_answer":"3"
+}
+,
+
+{
+"id":"4","question":"'.MOV' extension refers usually to what kind of file?",
+"possible_answers":"1, 2, 4, 3","correct_answer":"3"
+}
+,
+
+{
+"id":"5","question":"1 In which decade with the first transatlantic radio broadcast occur?",
+"possible_answers":"1, 4, 2, 3","correct_answer":"2"
+}
+,
+
+{
+"id":"6","question":"2 In which decade with the first transatlantic radio broadcast occur?",
+"possible_answers":"1, 4, 2, 3","correct_answer":"2"
+}
+,
+
+{
+"id":"7","question":"3 In which decade with the first transatlantic radio broadcast occur?",
+"possible_answers":"1, 4, 2, 3","correct_answer":"2"
+}
+,
+
+{
+"id":"8","question":"4 In which decade with the first transatlantic radio broadcast occur?",
+"possible_answers":"1, 4, 2, 3","correct_answer":"2"
+}
+
+]
+}
